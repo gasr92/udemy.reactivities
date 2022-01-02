8b0a1f91d0090746f35bb26d2f70ba1fc27d57a2
gsdr1992 <gabriel_silveira_r@hotmail.com>
2022-01-02 17:46:09 -0300

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,35 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
-
         [HttpGet]
         public async Task<ActionResult<IList<Activity>>> GetActivitities()
         {
-            var result = await _context.Activities.ToListAsync();
+            var result = await Mediator.Send(new List.Query());
             return result;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivitityById(Guid id)
         {
-            var result = await _context.Activities.FindAsync(id);
-            return result;
+            return await Mediator.Send(new Details.Query { Id = id } );
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+        {
+            var result = await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok(result);
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
+        {
+            activity.Id = id;
+            var result = await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            var result = await Mediator.Send(new Delete.Command { Id = id });
+            return Ok(result);
+        }
+
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -3,25 +3,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using Microsoft.Extensions.Configuration;
 using System;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "API", Version = "v1" });
-});
-
-var connString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DataContext>(opt =>
-    opt.UseSqlite(connString)
-);
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
